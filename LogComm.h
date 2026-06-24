a76f3da098e076e505838ab3c10780b0d6161dd6
德州扑克源代码_德州游戏源码_德州扑克源码开发_聊天通讯软件 <develop@deeptexas.ai>
2026-06-24 11:04:31 +0800

    Add files via upload
    server code

--- LogComm.h
+++ LogComm.h
@@ -1,5 +1,5 @@
-#ifndef __HALL_LOGCOMM_H__
-#define __HALL_LOGCOMM_H__
+#ifndef __LOGCOMM_H__
+#define __LOGCOMM_H__
 
 //
 #include <util/tc_logger.h>
@@ -23,75 +23,6 @@ using namespace tars;
 
 //配置信息
 #define FDLOG_CONFIG_INFO (FDLOG("config_info") << "|")
-
-//
-#define TERMINATE_APPLICATION() {sleep(10);terminate();}
-
-//金币变化日志
-#define COIN_CHANGE_LOG_TOPIC "coin_change_log"
-#define FDLOG_COIN_CHANGE_LOG (FDLOG("coin_change_log") << "|")
-
-//物品变化日志
-#define PROPS_CHANGE_LOG_TOPIC "props_change_log"
-#define FDLOG_PROPS_CHANGE_LOG (FDLOG("props_change_log") << "|")
-
-//积分变化日志
-#define POINT_CHANGE_LOG_TOPIC "point_change_log"
-#define FDLOG_POINT_CHANGE_LOG (FDLOG("point_change_log") << "|")
-
-//玩家基础信息日志
-#define USER_CHANGE_LOG_TOPIC "user_change_log"
-#define FDLOG_USER_CHANGE_LOG (FDLOG("user_change_log") << "|")
-
-//保险箱变化日志
-#define SAFES_CHANGE_LOG_TOPIC "safes_change_log"
-#define FDLOG_SAFES_CHANGE_LOG (FDLOG("safes_change_log") << "|")
-
-//
-#define FDLOG_INIT_FORMAT(x,y,z) (TarsTimeLogger::getInstance()->initFormatWithType<LogByMinute>(x,y,z))
-#define FDLOG_COIN_CHANGE_LOG_FORMAT (FDLOG_INIT_FORMAT("coin_change_log", "%Y%m%d%H%M", 5))
-#define FDLOG_PROPS_CHANGE_LOG_FORMAT (FDLOG_INIT_FORMAT("props_change_log", "%Y%m%d%H%M", 5))
-#define FDLOG_USER_CHANGE_LOG_FORMAT (FDLOG_INIT_FORMAT("user_change_log", "%Y%m%d%H%M", 5))
-
-//取收件箱数据请求
-#define FDLOG_GET_MAIL_DATA_INFO (FDLOG("get_mail_data_info") << "|")
-//发送邮件请求
-#define FDLOG_SEND_MAIL_INFO (FDLOG("send_mail_info") << "|")
-//收邮件请求
-#define FDLOG_RECEIVE_MAIL_INFO (FDLOG("receive_mail_info") << "|")
-//赠送货币日志
-#define FDLOG_AWARDS_PRIZES_INFO (FDLOG("awards_prizes_info") << "|")
-
-//充值日志
-#define RECHARGE_LOG_TOPIC "recharge_log"
-#define FDLOG_RECHARGE_LOG (FDLOG("recharge_log") << "|")
-#define FDLOG_RECHARGE_LOG_FORMAT (FDLOG_INIT_FORMAT("recharge_log", "%Y%m%d%H%M", 5))
-
-template<typename T>
-std::string toString(T t) 
-{
-	ostringstream os;
-	os << t;
-	return os.str();
-}
-
-template<typename T, typename... Args>
-std::string toString(T head, Args... args) 
-{
-	ostringstream os;
-	os << head;
-	return os.str() + toString(args...);
-}
-
-////
-#define THROW_LOGIC_ERROR(...) toString("[", __FILE__, ":" , __LINE__ , ":" , __FUNCTION__ , "] ", __VA_ARGS__)
-
-//接口性能边界值
-#define COST_MS 100
-
-//函数调用消耗时间
-#define FUNC_COST_MS(consumStartMs) { long __costTime__ = (TNOWMS) - (consumStartMs);\
- if (__costTime__ > 10) {ROLLLOG_WARN << "scheduler consumTime: " << __costTime__ << endl;} }
 
 #endif
 
--- OuterFactoryImp.h
+++ OuterFactoryImp.h
@@ -7,48 +7,30 @@
 #include "servant/Application.h"
 #include "globe.h"
 #include "OuterFactory.h"
-#include "util/tc_hash_fun.h"
 
 //wbl
 #include <wbl/regex_util.h>
 
-//服务代理
+//配置服务
 #include "ConfigServant.h"
-#include "DBAgentServant.h"
-#include "PushServant.h"
-#include "Log2DBServant.h"
+#include "HallServant.h"
 #include "GlobalServant.h"
-#include "GameRecordServant.h"
-#include "GameRecordProto.h"
-#include "LogComm.h"
+#include "PushServant.h"
 
 //
 using namespace config;
-using namespace dataproxy;
-using namespace dbagent;
+using namespace mail;
+using namespace global;
+using namespace hall;
+using namespace GoodsManager;
 
 //时区
-//#define ONE_DAY_TIME (24*60*60)
-//#define ZONE_TIME_OFFSET (8*60*60)
+#define ONE_DAY_TIME (24*60*60)
+#define ZONE_TIME_OFFSET (8*60*60)
 
 //
 class OuterFactoryImp;
 typedef TC_AutoPtr<OuterFactoryImp> OuterFactoryImpPtr;
-
-//玩家初始化财富
-typedef struct _UserInitWealth
-{
-    long gold;      //金币
-    long diamond;   //钻石
-    long score;     //积分
-    long ticketNum; //奖券
-} UserInitWealth;
-
-//平台类型
-typedef struct _TPlatformType
-{
-    map<int, string> mapPlatform;
-} PlatformType;
 
 /**
  * 外部工具接口对象工厂
@@ -67,8 +49,9 @@ private:
     ~OuterFactoryImp();
 
     //
-    friend class HallServantImp;
-    friend class HallServer;
+    friend class GMServantImp;
+    //
+    friend class GMServer;
 
 public:
     //框架中用到的outer接口(不能修改):
@@ -85,118 +68,68 @@ public:
 public:
     //加载配置
     void load();
-
     //代理配置
     void readPrxConfig();
+    //
     void printPrxConfig();
-    //道具配置
-    void readPropsConfig();
-    void printPropsConfig();
-    //经验配置
-    void readExperienceConfig();
-    void printExperienceConfig();
-
-public:
-    //
-    void asyncPushPropsInfo( map<long, push::PropsChangeNotify>& mapNotify);
-    //日志入库
-    void asyncLog2DB(const int64_t uid, const DaqiGame::TLog2DBReq &req);
-    //
-    void asyncLog2DB(const int64_t uid, const short type, const std::vector<string> &fields);
 
 private:
-    /**
-     *
-    */
+    //
     void createAllObject();
-
-    /**
-     *
-    */
+    //
     void deleteAllObject();
 
-public: // 获取代理
-    //游戏配置服务代理
-    const ConfigServantPrx getConfigServantPrx();
-    //数据库代理服务代理
-    const DBAgentServantPrx getDBAgentServantPrx(const int64_t uid);
-    //数据库代理服务代理
-    const DBAgentServantPrx getDBAgentServantPrx(const string &key);
-    //PushServer代理
-    const push::PushServantPrx getPushServerPrx(const int64_t uid);
-    //日志入库服务代理
-    const DaqiGame::Log2DBServantPrx getLog2DBServantPrx(const int64_t uid);
-    //全局服务代理
-    const global::GlobalServantPrx getGlobalServantPrx(const int64_t uid);
+public:
+    // 获取代理
+    const config::ConfigServantPrx getConfigServantPrx();
     //
-    const gamerecord::GameRecordServantPrx getGameRecordServantPrx(const int64_t uid);
+    const hall::HallServantPrx getHallServantPrx(const long uid);
+        //
+    const hall::HallServantPrx getHallServantPrx(const string key);
+    //
+    const global::GlobalServantPrx getGlobalServantPrx(const long uid);
+    //PushServer代理
+    const push::PushServantPrx getPushServerPrx(const long uid);
 
 public:
-    //拆分字符串成整形
-    int splitInt(string szSrc, vector<int> &vecInt);
-    //
-    int parseProps(string szSrc, map<string, string>& result);
-    //
-    config::E_Props_Type getPropsTypeById(const int propsId);
-    //
-    string getPropsColById(const int propsId);
-    //
-    int getPropsIdByCol(const string colName);
-    int getPropsInfoConfig(const int propsId, config::PropsInfoCfg &cfg);
-    //
-    int getExpMaxLevel();
-    //
-    int getExpUpgByLevel(const int iLevel);
-    //
-    int getExpChangeByType(const config::E_EXP_CHANGE_TYPE eChangeType);
-    //去除特殊字符
-    bool filterUtf8(unsigned char *str, int length);
-    //检查字符串长度跟特殊字符
-    bool checkStrUtf8(const string &src);
-    // 取固定长度字符串
-    string subStrUtf8(const string &src, size_t bytes);
-    // 计算utf8字符偏移量
-    int utf8_char_len(char firstByte);
-    // 获得时间秒数
-    int getTimeTick(const string &str);
+    //格式化时间
+    string GetTLogTimeFormat();
 
 private:
-    //读写锁，防止脏读
+    //拆分字符串成整形
+    int splitInt(string szSrc, vector<int> &vecInt);
+
+private:
+    //域名解析
+    void getIp(char *domain, char *ip);
+    //域名解析
+    string getIp(const string &domain);
+
+private:
     wbl::ReadWriteLocker m_rwlock;
 
 private:
     //框架用到的共享对象(不能修改):
     tars::TC_Config *_pFileConf;
-    tars::TC_Config *_pFileGoodsList;//道具配置
+
     OuterProxyFactoryPtr _pProxyFactory;
 
-private: //业务自有的共享对象:
-    //配置服务
+private:
+    //业务自有的共享对象:
+    //...
+    // 用户信息
+    std::string _HallServantObj;
+    HallServantPrx _HallServerPrx;
+
+    std::string _GlobalServantObj;
+    global::GlobalServantPrx _GlobalServantPrx;
+
     std::string _ConfigServantObj;
-    ConfigServantPrx _ConfigServerPrx;
-    //数据库服务
-    std::string _DBAgentServantObj;
-    DBAgentServantPrx _DBAgentServerPrx;
+    ConfigServantPrx _ConfigServantPrx;
+
     //推送服务
     string _sPushServantObj;
     push::PushServantPrx _pushServerPrx;
-    //日志服务
-    std::string _Log2DBServantObj;
-    DaqiGame::Log2DBServantPrx _Log2DBServerPrx;
-    //全局服务
-    std::string _GlobalServantObj;
-    global::GlobalServantPrx _GlobalServerPrx;
-    //
-    std::string _GameRecordServantObj;
-    gamerecord::GameRecordServantPrx _GameRecordServerPrx;
-
-public:
-    config::ListPropsInfoCfgResp mapPropsConfig;
-
-    config::ListExpUpgCfgResp listExpUpgCfgResp;
-
-    config::ListExpChangeCfgResp listExpChangeCfgResp;
-
 };
 
 ////////////////////////////////////////////////////////////////////////////////
